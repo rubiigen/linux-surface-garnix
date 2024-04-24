e4ca9018c443c5289fbdd98b68f6b58a34205ed9
rubiigen <alyxwren@proton.me>
2024-04-24 23:47:12 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs }: {
+  };
+}
